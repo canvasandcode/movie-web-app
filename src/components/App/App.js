fd1f56553e514dfa2d84600dfedd8d3b8289d587
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-14 20:12:24 +0100

    creating header with placeholder image

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../elements/Header/Header';
+import Home from '../Home/Home';
+
+const App = () => {
+    //every component in React needs to return something
+    return (
+        //Wrap in 1 element, or React Fragments
+        <div>
+            <Header/>
+            <Home/>
+        </div>
+    )
+}
+
+export default App;
