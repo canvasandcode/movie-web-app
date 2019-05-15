08fd74423caf465710adbdf64a68c9faa464e23d
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-15 19:51:13 +0100

    creating components

---
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadMoreBtn.css';
+
+const MovieThumb = (props) => {
+    return (
+        <div>
+            Load More
+        </div>
+    )
+}
+
+export default LoadMoreBtn;
