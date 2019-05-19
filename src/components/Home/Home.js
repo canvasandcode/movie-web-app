d3e42e584a05521849f53f1b3d28cba5c50fbe74
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-19 13:05:40 +0100

    setting up config for api, fetching data, and adding logic for load more button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from "../../config"
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -10,7 +11,50 @@ import './Home.css';
 
 class Home extends Component {
     state = {
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: ''
+    }
 
+    //life cycle method - will fire when component has mounted
+    componentDidMount() {
+        this.setState({ loading: true });
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    loadMoreItems = () => {
+        //2 different API URls - if doing a search we want more searched movies, if we are doing load more - we want more popular movies
+        let endpoint = ''
+        this.setState({loading: true});
+
+        //If search term is empty than we know we're not searching for anything so the API url will change to show next list of popular movies
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-us&page=${this.state.currentPage + 1}`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-us&query${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+        }
+        //this method will fetch the next page when clicking on load more
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            this.setState ({
+                //append new movies to any old movies using spread syntax 
+                movies: [...this.state.movies, ...result.results],
+                //if heroimage is null then show image from first result
+                heroImage: this.state.heroImage || result.results[0],
+                loading: false,
+                currentPage: result.page,
+                totalpages: result.total_pages
+            })
+        })
     }
 
     render() {
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = '9ce0c4dd8691de512d95ba1799755985';
 
 // Images
 // An image URL looks like this example:
