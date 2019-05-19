d75ed9dc253c9fef5bda6a1f4990b988720ba150
Imran Rahman <imran@imrans-mac-mini.mynet>
2019-05-19 22:30:16 +0100

    adding zoom animation on page scroll to movie elements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config';
-import HeroImage from '../elements/HeroImage/HeroImage';
-import SearchBar from '../elements/SearchBar/SearchBar';
-import FourColGrid from '../elements/FourColGrid/FourColGrid';
-import MovieThumb from '../elements/MovieThumb/MovieThumb';
-import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
-import Spinner from '../elements/Spinner/Spinner';
-import './Home.css';
+import React, { Component } from "react";
+import {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  POSTER_SIZE,
+  BACKDROP_SIZE
+} from "../../config";
+import HeroImage from "../elements/HeroImage/HeroImage";
+import SearchBar from "../elements/SearchBar/SearchBar";
+import FourColGrid from "../elements/FourColGrid/FourColGrid";
+import MovieThumb from "../elements/MovieThumb/MovieThumb";
+import LoadMoreBtn from "../elements/LoadMoreBtn/LoadMoreBtn";
+import Spinner from "../elements/Spinner/Spinner";
+
+import "./Home.css";
 
 class Home extends Component {
   state = {
@@ -15,8 +22,8 @@ class Home extends Component {
     loading: false,
     currentPage: 0,
     totalPages: 0,
-    searchTerm: ''
-  }
+    searchTerm: ""
+  };
 
   //life cycle method - will fire when component has mounted
   componentDidMount() {
@@ -25,89 +32,101 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
-  searchItems = (searchTerm) => {
+  searchItems = searchTerm => {
     console.log(searchTerm);
-    let endpoint = '';
+    let endpoint = "";
     this.setState({
       movies: [],
       loading: true,
       searchTerm
-    })
+    });
 
-    if (searchTerm === '') {
+    if (searchTerm === "") {
       endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
     } else {
       endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
     }
     this.fetchItems(endpoint);
-
-  }
+  };
 
   loadMoreItems = () => {
     //2 different API URls - if doing a search display searched movies, if we are doing load more - show more popular movies
-    let endpoint = '';
+    let endpoint = "";
     this.setState({ loading: true });
 
     //If search term is empty than we know we're not searching for anything so the API url will change to show next list of popular movies
-    if (this.state.searchTerm === '') {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
     } else {
-      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${
+        this.state.searchTerm
+      }&page=${this.state.currentPage + 1}`;
     }
     //this method will fetch the next page when clicking on load more
     this.fetchItems(endpoint);
-  }
+  };
 
-  fetchItems = (endpoint) => {
+  fetchItems = endpoint => {
     fetch(endpoint)
-    .then(result => result.json())
-    .then(result => {
-      this.setState({
-        //append new movies to any old movies using spread syntax 
-        movies: [...this.state.movies, ...result.results],
-        //if heroimage is null then show image from first result
-        heroImage: this.state.heroImage || result.results[0],
-        loading: false,
-        currentPage: result.page,
-        totalPages: result.total_pages
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          //append new movies to any old movies using spread syntax
+          movies: [...this.state.movies, ...result.results],
+          //if heroimage is null then show image from first result
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
       })
-    })
-    .catch(error => console.error('Error:', error))
-  }
+      .catch(error => console.error("Error:", error));
+  };
 
   render() {
     return (
       <div className="home">
-      {this.state.heroImage ?
-        <div>
-          <HeroImage
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
-            title={this.state.heroImage.original_title}
-            text={this.state.heroImage.overview}
-          />
-          <SearchBar callback={this.searchItems} />
-        </div> : null }
+        {this.state.heroImage ? (
+          <div>
+            <HeroImage
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${
+                this.state.heroImage.backdrop_path
+              }`}
+              title={this.state.heroImage.original_title}
+              text={this.state.heroImage.overview}
+            />
+            <SearchBar callback={this.searchItems} />
+          </div>
+        ) : null}
         <div className="home-grid">
           <FourColGrid
-            header={this.state.searchTerm ? 'Search Result' : 'Popular Movies'}
+            header={this.state.searchTerm ? "Search Result" : "Popular Movies"}
             loading={this.state.loading}
-            >
-            {this.state.movies.map ( (element, i) => {
-              return <MovieThumb
-                        key={i}
-                        image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
-                        movieId={element.id}
-                        movieName={element.original_title}
-                     />
+          >
+            {this.state.movies.map((element, i) => {
+              return (
+                <MovieThumb
+                  key={i}
+                  image={
+                    element.poster_path
+                      ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}`
+                      : "./images/no_image.jpg"
+                  }
+                  movieId={element.id}
+                  movieName={element.original_title}
+                />
+              );
             })}
           </FourColGrid>
           {this.state.loading ? <Spinner /> : null}
-          {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ?
+          {this.state.currentPage <= this.state.totalPages &&
+          !this.state.loading ? (
             <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} />
-            : null }
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import './FourColGrid.css';
+import React from "react";
+import Zoom from "react-reveal/Zoom";
+import "./FourColGrid.css";
 
-const FourColGrid = (props) => {
-
+const FourColGrid = props => {
   const renderElements = () => {
     //create a new array of elements (each grid element)
-    const gridElements = props.children.map( (element, i) => {
+    const gridElements = props.children.map((element, i) => {
       return (
-        <div key={i} className="grid-element">
-          {element}
-        </div>
-      )
-    })
+        <Zoom>
+          <div key={i} className="grid-element">
+            {element}
+          </div>
+        </Zoom>
+      );
+    });
     return gridElements;
-  }
+  };
 
   return (
     <div className="grid">
-    {/* if props not empty then show header text */}
+      {/* if props not empty then show header text */}
       {props.header && !props.loading ? <h1>{props.header}</h1> : null}
-      <div className="grid-content">
-        {renderElements()}
-      </div>
+      <div className="grid-content">{renderElements()}</div>
     </div>
-  )
-}
+  );
+};
 
 export default FourColGrid;
